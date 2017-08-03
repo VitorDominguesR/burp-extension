f3f6faeb5bd32c575395f2ae384c418b8b7d3ba2
VDR <vitor_dmrb@hotmail.com>
2017-08-03 11:16:23 -0300

    Bugs Fixed

--- PT-Manager-master/PTManager.py
+++ PT-Manager-master/PTManager.py
@@ -614,10 +614,13 @@ class BurpExtender(IBurpExtender, ITab, IMessageEditorController, AbstractTableM
         transformer.transform(source, result)
 
     def generateReport(self, event):
+        n = None
         if self.reportType.getSelectedItem() == "HTML":
             path = self.reportToHTML()
+            n = 1
         if self.reportType.getSelectedItem() == "XLSX":
             path = self.reportToXLS()
+            n = 1
         if self.reportType.getSelectedItem() == "DOCX":
             vulnerabilidades = self.sortVul(self._log)
             no_urls_listed = []
@@ -634,6 +637,7 @@ class BurpExtender(IBurpExtender, ITab, IMessageEditorController, AbstractTableM
                 else:
                     self.popup(u'Relatório não foi gerado')
             else:
+                n=1
                 templates = os.path.dirname(
                     os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/templates_pt_manager"
                 path = self.generateReportFromDocxTemplate(templates,
@@ -657,6 +661,7 @@ class BurpExtender(IBurpExtender, ITab, IMessageEditorController, AbstractTableM
                 else:
                     self.popup(u'Relatório não foi gerado')
             else:
+                n=1
                 templates = os.path.dirname(
                     os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/templates_pt_manager"
                 path = self.generateReportFromDocxTemplate(templates,
@@ -664,12 +669,12 @@ class BurpExtender(IBurpExtender, ITab, IMessageEditorController, AbstractTableM
                                                            externo=True)
         if self.reportType.getSelectedItem() == "XML":
             path = self.generateXMLReport()
-
-        y = JOptionPane.showConfirmDialog(None,
-                                          "Report generated successfuly:\n%s\nWould you like to open it?" % ('Project %s'%self.projName.getText()),
-                                          "EGV Manager", JOptionPane.YES_NO_OPTION)
-        if y == JOptionPane.YES_OPTION:
-            self.openFolderOrFile(path)
+        if n != None and n != JOptionPane.NO_OPTION:
+            y = JOptionPane.showConfirmDialog(None,
+                                              "Report generated successfuly:\n%s\nWould you like to open it?" % ('Project %s'%self.projName.getText()),
+                                              "EGV Manager", JOptionPane.YES_NO_OPTION)
+            if y == JOptionPane.YES_OPTION:
+                self.openFolderOrFile(path)
 
     def generateXMLReport(self):
         try:
